f099fbed7821adbf5316542fc9bc52832483c070
Jason Pierna <siilver777@me.com>
2016-12-11 18:20:45 +0100

    Fetch JSON list of pokemon in custom tableview

--- PokeStats/ListViewController.swift
+++ PokeStats/ListViewController.swift
@@ -8,18 +8,62 @@
 
 import UIKit
 
+import Alamofire
+import SwiftyJSON
+
 class ListViewController: UITableViewController {
+    
+    var pokemons = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        fetchPokemons()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: - UITableViewDataSource
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pokemons.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PokemonTableViewCell", for: indexPath) as? PokemonTableViewCell else {
+            return PokemonTableViewCell(style: .default, reuseIdentifier: "PokemonTableViewCell")
+        }
+        
+        cell.pokemonNameLabel.text = pokemons[indexPath.row]
+        
+        return cell
+    }
+    
+    // MARK: - Misc
+    
+    func fetchPokemons() {
+        Alamofire.request(API.pokemons).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value).arrayValue
+                
+                for pokemon in json {
+                    if let name = pokemon["name"].string {
+                        self.pokemons.append(name)
+                    }
+                }
+                self.tableView.reloadData()
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
 
