08f7d105bec341e9f0b38a74400a124006de9b7e
Jason Pierna <siilver777@me.com>
2016-12-11 18:20:06 +0100

    Adds Models (Pokémon, Type, Stats)

---
+++ PokeStats/Stats.swift
@@ -0,0 +1,18 @@
+//
+//  Stats.swift
+//  PokeStats
+//
+//  Created by Jason Pierna on 11/12/2016.
+//  Copyright © 2016 Jason Pierna. All rights reserved.
+//
+
+import Foundation
+
+struct Stats {
+    var pv: Int
+    var atk: Int
+    var def: Int
+    var atkspe: Int
+    var defspe: Int
+    var vit: Int
+}
---
+++ PokeStats/Type.swift
@@ -0,0 +1,13 @@
+//
+//  Type.swift
+//  PokeStats
+//
+//  Created by Jason Pierna on 11/12/2016.
+//  Copyright © 2016 Jason Pierna. All rights reserved.
+//
+
+import Foundation
+
+enum Type: Int {
+    case acier = 1, combat, dragon, eau, fee, feu, glace, insecte, normal, plante, poison, psy, roche, sol, spectre, tenebres, vol, electrik
+}
