6e11f978ff7e2d9030108f963a4574b2105f42cd
Jason Pierna <siilver777@me.com>
2016-12-11 18:19:09 +0100

    API class updated

--- PokeStats/API.swift
+++ PokeStats/API.swift
@@ -14,62 +14,46 @@ import Foundation
 class API {
     
     /**
-     The base URL lets us construct other PokéAPI routes easily.
+     The base URL lets us construct other routes easily.
     */
-    private static let baseURL = "http://pokeapi.co/api/v2/"
+    private static let baseURL = "http://192.168.1.54:8080/"
     
     /**
-     The Pokémon URL without parameters can fetch a list of 20 Pokemons with names and url.
+     The Pokémon URL fetch a list of every Pokémon.
      */
-    private static var pokemon: String {
-        return baseURL + "pokemon/"
+    public static var pokemons: String {
+        return baseURL + "pokemons/"
     }
     
     /**
-     The Pokémon URL with the limit parameters can fetch more than 20 Pokémons with names and url.
-     - parameter limit: The fetch limit ([1; 811]).
-    */
-    public static func pokemonList(limit: Int) -> String {
-        return pokemon + "?limit=" + String(limit)
-    }
-    
-    /**
-     The Pokémon URL with the limit and offset parameters can fetch more than 20 Pokémons with names and url, starting with an other Pokémon than Bulbasaur.
-     - parameter limit: The fetch limit ([1; 811]).
-     - parameter offset: The offset to start the list ([1; 811]).
-    */
-    public static func pokemonList(limit: Int, offset: Int) -> String {
-        return pokemonList(limit: limit) + "&offset=" + String(offset)
-    }
-    
-    /**
-     This Pokémon URL lets us fetch most details about a specific Pokémon.
+     This Pokémon URL lets us fetch details about a specific Pokémon.
      - parameter no: The ID of the Pokémon (National Pokédex numeroration).
     */
     public static func pokemon(no: Int) -> String {
-        return pokemon + String(no) + "/"
+        return pokemons + String(no) + "/"
     }
     
     /**
-     The Pokémon Species URL without parameters can fetch a list of 20 Pokemons species with names and url.
-    */
-    private static var pokemonSpecies: String {
-        return baseURL + "pokemon_species/"
+     The Artwork Route lets us fetch the official artwork of a Pokémon.
+     - parameter no: The ID of the Pokémon (National Pokédex numeroration).
+     */
+    public static func artwork(no: Int) -> String {
+        return baseURL + "artworks/" + String(no) + "/"
     }
     
     /**
-     This Pokémon Species URL lets us fetch details about a specific Pokémon specie.
+     The Miniature Route lets us fetch the miniature of a Pokémon.
+     - parameter no: The ID of the Pokémon (National Pokédex numeroration).
+     */
+    public static func mini(no: Int) -> String {
+        return baseURL + "minis/" + String(no) + "/"
+    }
+    
+    /**
+     The Sound Route lets us fetch the sound of a Pokémon.
       - parameter no: The ID of the Pokémon (National Pokédex numeroration).
      */
-    public static func pokemonSpecies(no: Int) -> String {
-        return pokemonSpecies + String(no) + "/"
-    }
-    
-    /**
-     The Shout Route lets us fetch the sound of a Pokémon on PokeCries.com.
-      - parameter no: The ID of the Pokémon (National Pokédex numeroration).
-     */
-    public static func shout(no: Int) -> String {
-        return "http://pokecries.com/sound/" + String(no) + ".mp3"
+    public static func sound(no: Int) -> String {
+        return baseURL + "sounds/" + String(no) + "/"
     }
 }
