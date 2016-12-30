198745d277e8a1117c6cd04443d77143e2eb9725
Jason Pierna <siilver777@me.com>
2016-12-30 15:23:39 +0100

    Adds Settings View Controller with Facebook SDK Login support

---
+++ PokeStats/SettingsTableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsTableViewController.swift
+//  PokeStats
+//
+//  Created by Jason Pierna on 30/12/2016.
+//  Copyright © 2016 Jason Pierna. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+
+class SettingsTableViewController: UITableViewController {
+    
+    @IBOutlet weak var loginButton: FBSDKLoginButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
